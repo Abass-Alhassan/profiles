7c806a877408d7098992171ae495d11b26b51f49
Abass-Alhassan <icewrldspacely@gmail.com>
2022-03-16 08:17:01 -0700

    first commit

---
+++ src/writers.js
@@ -0,0 +1,38 @@
+const writers=[
+    {
+        id:1,
+        name: "jamila lorde",
+        avatar:"audre_lorde",
+        email:"jamila23@gmail.com",
+        phone:"0546282732"
+    },
+    {
+        id:2,
+        name: "clifford naaeder",
+        avatar:"belle_hooks",
+        email:"clifford23@gmail.com",
+        phone:"054666666"
+    },
+    {
+        id:3,
+        name: "dari joseph",
+        avatar:"du_bois",
+        email:"joseph23@gmail.com",
+        phone:"0546282111"
+    },
+    {
+        id:4,
+        name: "slim shady",
+        avatar:"james_baldwin",
+        email:"slim23@gmail.com",
+        phone:"0546282000"
+    },
+    {
+        id:5,
+        name: "abass alhassan",
+        avatar:"maya_angelou",
+        email:"abass23@gmail.com",
+        phone:"0546236373"
+    }
+]
+export default writers;
